1a04ad6a74db0b0fa0d5e4f9eaba2db6cea28865
Abu <aeam@queensu.ca>
2019-09-05 01:48:50 -0400

    big send

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+data = {'description':"Creatively Resourceful Mechanical Engineering student with a strong passion for technology, network security, and prototype development. I love breaking and building IT!",}
 @app.route('/')
 def hello():
-    return render_template('base.html')
+    return render_template('base.html',data=data)
 
 @app.route('/form')
 def form():
