9cae7323c528dd83e7361f219d3dc3d81debdad2
Abu <aeam@queensu.ca>
2019-09-06 13:52:56 -0400

    ayyy?

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
+from portfoliodb import data, Design
 from flask import Flask, render_template
 app = Flask(__name__)
 
-data = {'description':"Creatively Resourceful Mechanical Engineering student with a strong passion for technology, network security, and prototype development. I love breaking and building IT!",}
+
 @app.route('/')
 def hello():
-    return render_template('base.html',data=data)
+    return render_template('base.html', data=data, Design=Design)
+
 
 @app.route('/form')
 def form():
     return render_template('form.html')
+
 
 @app.route('/application')
 def application():
---
+++ portfoliodb.py
@@ -0,0 +1,116 @@
+
+data = {'description':
+        "Creatively Resourceful Mechanical Engineering student and Cybersecurity professional with a strong passion for technology, network security, and prototype development. I love breaking and building IT!",
+        'Development & Technical Skills': {
+            'Matlab': '70',
+            'Solid Work/Edge': '70',
+            'Python': '85',
+            'Flask': '75',
+            'JavaScript': '65',
+            'Node.js': '60',
+            'Arduino': '80',
+            'RaspberryPi': '90',
+            'Front-end Web Development': '55',
+            'Back-end Web Development': '70',
+            'Drupal': '50',
+            'API prigramming': '70',
+            'Linux': '90',
+            'SQL': '60',
+            'MongoDB(NoSQL)': '70',
+            'Microsoft Azure Cloud Architecture': '60',
+            'Tenable Nessus': '95',
+            'AlienVault': '85'
+        },
+        'Industry Knowledge': {
+            'Information Technology': '85',
+            'Network Architecture and Security': '65',
+            'Web Development': '75',
+            'Data Science': '50',
+            'Manufacturing': '50',
+            'Electronics': '50',
+            'BlockChain': '45',
+            'Transportation': '30',
+            'Library and Information Services': '40',
+        },
+        'Professional Skills': {
+            'Time Management': '70',
+            'Autonomy': '90',
+            'Conflict Resolution': '90',
+            'Project Management': '75',
+            'Communication': '80',
+            'Decision Making': '80'
+        },
+        'Work': {
+            "Queen's University": {
+                "Information Security Associate": {
+                    'Period': "May 2019 - Present",
+                    'Responsibilities':
+                        ["Monitored the University’s networks for security breaches and helped investigate a violation when one occurred",
+                         "Conducted web application scans, penetration tests and network security audits using Tenable Nessus and Fortify WebInspect; and issued tickets for remediation",
+                         "Developed solutions to automate security operations",
+                         "Planned Data Classification Standard with counsel of the Information Security Office and the University Secretariat and Legal Counse to satisfy security standards and advocate safe web practices across the university",
+                         "Developed a web application and wrote up a Standard Operating Procedure(SOP) for the Data Classification Standard tool",
+                         "Familiar with the NIST Cybersecurity framework and NIST Cloud Computing Security Reference Architecture",
+                         "Familiar with the Queen’s Information Security Policies, guidelines, and procedures",
+                         "Conducted daily PCI network checks to ensure compliance with the PCI Data Security Standard",
+                         "Completed the CompTIA Security+ and Network+ courses. Sat the Security+ exam and earned the certification"
+                         ]
+                },
+                "Library Assistant": {
+                    'Period': "November 2016 - May 2019",
+                    'Responsibilities': ["Handled the circulation desk and took responsibility for the library's standard operations",
+                                         "Assisted patrons in the usage of printers, scanners, and other library media equipment while responding to directional and locational inquiries regarding resource materials",
+                                         "Worked with a team to provide guidance to students seeking assistance with learning strategies and further resource help within the library system"]
+                }
+            },
+            "Queen's Hyperloop Design Team": {
+                "Prototype team member": {
+                    'Period': "November 2016 - April 2018",
+                    'Responsibilities': ["Involved in Electronics subteam",
+                                         "Ported existing 'Arduino + Particle Photon + Nodejs server' stack to work on a single RaspberryPi-zero, with an added camera"]
+                }
+            },
+            "Serendib Toastmasters Gavel Club": {
+                "President": {
+                    'Period': 'May 2011 - June 2012'
+                },
+                "Vice President Education": {
+                    'Period': 'May 2010 - June 2011'
+                },
+                "Secretary": {
+                    'Period': 'May 2009 - June 2010'
+                }
+            }
+        },
+        }
+
+Design = [{'Project Name': 'MECH 323 - Gearbox Design',
+           'description': 'Construction of a gearbox designed for specific performace outcomes.',
+           'tasks': ['Group-work scenario',
+                     'Competitively oriented designing',
+                     '3D modeling experience',
+                     '3D printing experience']},
+          {'Project Name': "Queen's Hyperloop - Software and Electronics Prototyping",
+           'description': 'Moved the existing prototype electronics stack from "Sensors + Arduino + Particle Photon + separate Nodejs server" to a "Sensors+Camera+RaspberryPi-zero" stack',
+           'tasks': ['Collaborated with Prototyping team to meet their needs',
+                     '3D printing experience',
+                     'Arduino programming',
+                     'Arduino programming',
+                     'IOT experience',
+                     'JavaScript: Node.js and Johnny-5 libraries experience']},
+          {'Project Name': "Queen's ITS - Data Classification Standard",
+           'description': 'Planned and Developed Data Classification Standard in collaboration with Information Security Office and the University Secretariat and Legal Counsel. The Standard was updated to satisfy security standards and advocate safe web practices accross the university.',
+           'tasks': ['Researched NIST Security Standards and Data Classification Scheme of Univeristies worldwide',
+                     'Drupal Web-Devolopment Experience',
+                     'MongoDB NoSQL Experience',
+                     'Developed Standard Operating Procedure']
+           },
+          {'Project Name': "Queen's ITS - Deny by Default Automation",
+           'description': "Developed a web service to automate Security Admin tasks like ticket creation, web application scanning, and firewall rule creation",
+           'tasks': ['Python experience',
+                     'Web Application development with Flask framework',
+                     'Bootstrap4 HTML/CSS Experience',
+                     'Tenable Nessus API',
+                     'Fortigate ForiOS API',
+                     'Azure Cloud Architecture']
+           }, ]
